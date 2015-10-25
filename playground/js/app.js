389f826b7feeebfacbd69a2753fe289d0ca2a99f
madcabbage <madcabbage@gmail.com>
2015-10-25 13:29:01 +0000

    added angular and angular route

---
+++ playground/js/app.js
@@ -0,0 +1,19 @@
+angular.module('PlaygroundPages', ['ngRoute'])
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+
+    .when('/about', {
+      templateUrl: 'templates/pages/about/index.html'
+    })
+    .when('/portfolio', {
+      templateUrl: 'templates/pages/portfilio/index.html'
+    })
+    .when('/',{
+		templateUrl: 'templates/pages/about/index.html'
+    })
+    .otherwise('/',{
+    	templateUrl: 'templates/pages/about/index.html'
+    })
+  ;
+
+}]);
